20d7ba6a6a7042cb2ae54f14f08aa496956a24c9
Ben Shaver <benshaver8@gmail.com>
2021-01-14 13:33:38 +0200

    Fixed configuration

--- client/my-react-client/src/config.js
+++ client/my-react-client/src/config.js
@@ -14,14 +14,8 @@ config.backend = {
 
 config.backend.url = `http://${config.backend.host}:${config.backend.port}`
 
-// config.auth_service = {
-//     issuer: 'http://localhost:8080/auth/realms/demorealm',
-//     client_id: 'change_me',
-//     client_secret: 'change_me'
-// }
-
 config.auth_service = {
-    issuer: 'http://localhost:8080/auth/realms/test',
-    client_id: 'test',
-    client_secret: '52178877-e00c-4d0f-b59e-f11ececa7223'
+    issuer: 'http://localhost:8080/auth/realms/demorealm',
+    client_id: 'change_me',
+    client_secret: 'change_me'
 }
